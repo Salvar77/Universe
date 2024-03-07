a6038958c8ef80128708b65ae7a5c2d0a6006eb0
Łukasz <lukaszkus77@gmail.com>
2024-03-07 09:16:41 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -7,124 +7,137 @@ const nextBtn = document.querySelector(".gallery-button.next");
 const galleryImages = document.querySelector(".gallery-images");
 const images = document.querySelectorAll(".gallery-images img");
 const footerYear = document.querySelector(".footer__year");
+const cards = document.querySelectorAll(".card");
 
 let imageIndex = 0;
 let currentIndex = 1;
 
 // Functions
 const handleNavItemsAnimation = () => {
-	let delayTime = 5;
-	
-	allNavItems.forEach((item) => {
-		item.classList.toggle(".nav-items-animation");
-		item.style.animationDelay = "." + delayTime + "s";
-		delayTime++;
-	});
+  let delayTime = 5;
+
+  allNavItems.forEach((item) => {
+    item.classList.toggle(".nav-items-animation");
+    item.style.animationDelay = "." + delayTime + "s";
+    delayTime++;
+  });
 };
 
 const handleNav = () => {
-	nav.classList.toggle("nav--active");
-	
-	allNavItems.forEach((item) => {
-		item.addEventListener("click", () => {
-			nav.classList.remove("nav--active");
-		});
-	});
-	
-	handleNavItemsAnimation();
+  nav.classList.toggle("nav--active");
+
+  allNavItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.remove("nav--active");
+    });
+  });
+
+  handleNavItemsAnimation();
 };
 
 const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
-	console.log(".footer-year");
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+  console.log(".footer-year");
 };
 
 handleCurrentYear();
 
 const changeSlide = (newIndex) => {
-	galleryImages.style.transition = "transform 0.4s ease-in-out";
-	galleryImages.style.transform = `translateX(-${(newIndex * 100) / 6}%)`;
+  galleryImages.style.transition = "transform 0.4s ease-in-out";
+  galleryImages.style.transform = `translateX(-${(newIndex * 100) / 6}%)`;
 };
 
 const handleTransitionEnd = () => {
-	galleryImages.removeEventListener("transitionend", handleTransitionEnd);
+  galleryImages.removeEventListener("transitionend", handleTransitionEnd);
 
-	if (currentIndex === 0) {
-		currentIndex = images.length;
-	} else if (currentIndex === images.length + 1) {
-		currentIndex = 1;
-	}
+  if (currentIndex === 0) {
+    currentIndex = images.length;
+  } else if (currentIndex === images.length + 1) {
+    currentIndex = 1;
+  }
 
-	galleryImages.style.transition = "none";
-	galleryImages.style.transform = `translateX(-${(currentIndex * 100) / 6}%)`;
-	requestAnimationFrame(() => {
-		requestAnimationFrame(() => {
-			galleryImages.style.transition = "transform 0.4s ease-in-out";
-		});
-	});
+  galleryImages.style.transition = "none";
+  galleryImages.style.transform = `translateX(-${(currentIndex * 100) / 6}%)`;
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      galleryImages.style.transition = "transform 0.4s ease-in-out";
+    });
+  });
 };
 
 const updateImageSources = () => {
-	const screenWidth = window.innerWidth;
-	const galleryImages = document.querySelectorAll(".gallery-images img");
+  const screenWidth = window.innerWidth;
+  const galleryImages = document.querySelectorAll(".gallery-images img");
 
-	galleryImages.forEach((image) => {
-		let newSrc;
+  galleryImages.forEach((image) => {
+    let newSrc;
 
-		if (screenWidth >= 992) {
-			newSrc = image.src.replace("-640.webp", "-1920.webp");
-		} else {
-			newSrc = image.src.replace("-1920.webp", "-640.webp");
-		}
+    if (screenWidth >= 992) {
+      newSrc = image.src.replace("-640.webp", "-1920.webp");
+    } else {
+      newSrc = image.src.replace("-1920.webp", "-640.webp");
+    }
 
-		image.src = newSrc;
-	});
+    image.src = newSrc;
+  });
 };
+
+const handleCardClick = (event) => {
+  // Tutaj logika obracania karty lub inna akcja, np.:
+  const card = event.currentTarget;
+  card.classList.toggle("is-flipped"); // Zakładając, że masz taką klasę CSS do obracania karty
+};
+
+cards.forEach((card) => {
+  card.addEventListener("click", handleCardClick);
+  // Dodanie obsługi dotyku, jeśli jest potrzebna specyficzna logika dla urządzeń mobilnych
+  card.addEventListener("touchend", handleCardClick);
+});
 
 // Event Listeners
 document.addEventListener("DOMContentLoaded", () => {
-	const navElementsToHide = document.querySelectorAll(".nav__element-to-hide");
+  const navElementsToHide = document.querySelectorAll(".nav__element-to-hide");
 
-	window.addEventListener("scroll", () => {
-		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-		navElementsToHide.forEach((element) => {
-			if (scrollTop > 200) {
-				element.style.opacity = "0";
-			} else {
-				element.style.opacity = "1";
-			}
-		});
-	});
+    navElementsToHide.forEach((element) => {
+      if (scrollTop > 200) {
+        element.style.opacity = "0";
+      } else {
+        element.style.opacity = "1";
+      }
+    });
+  });
 
-	const hamburger = document.querySelector(".hamburger");
+  const hamburger = document.querySelector(".hamburger");
 
-	hamburger.addEventListener("click", () => {
-		hamburger.classList.toggle("is-active");
-	});
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+  });
 });
 
 hamburger.addEventListener("click", handleNav);
 
 prevBtn.addEventListener("click", () => {
-	if (currentIndex === 0) {
-		galleryImages.addEventListener("transitionend", handleTransitionEnd);
-		currentIndex = images.length;
-	} else {
-		currentIndex--;
-	}
-	changeSlide(currentIndex);
+  if (currentIndex === 0) {
+    galleryImages.addEventListener("transitionend", handleTransitionEnd);
+    currentIndex = images.length;
+  } else {
+    currentIndex--;
+  }
+  changeSlide(currentIndex);
 });
 
 nextBtn.addEventListener("click", () => {
-	if (currentIndex === images.length + 1) {
-		galleryImages.addEventListener("transitionend", handleTransitionEnd);
-		currentIndex = 1;
-	} else {
-		currentIndex++;
-	}
-	changeSlide(currentIndex);
+  if (currentIndex === images.length + 1) {
+    galleryImages.addEventListener("transitionend", handleTransitionEnd);
+    currentIndex = 1;
+  } else {
+    currentIndex++;
+  }
+  changeSlide(currentIndex);
 });
 
 // Initialize
@@ -138,7 +151,6 @@ galleryImages.append(firstClone);
 galleryImages.prepend(lastClone);
 
 galleryImages.style.transform = `translateX(-${(currentIndex + 1) * 100}vw)`;
-
 
 window.addEventListener("resize", updateImageSources);
 updateImageSources();
